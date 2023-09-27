e870c8d3d54de543779c54e6db1c8a2eb24116dd
ronycode <ronypc@outlook.com>
2023-09-27 19:37:07 -0300

    primeiro projeto arch

---
+++ src/Domains/User/Interface/UserRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Gso\Ws\Domains\User\Interface;
+
+
+use Gso\Ws\Domains\User\User;
+
+interface UserRepositoryInterface
+{
+    public function login(string $email, string $senha): User;
+
+    public function getUsuarioById(int $codUsuario): User;
+}
