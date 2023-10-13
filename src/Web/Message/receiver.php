afda25f4b8f7234ab03c1ad52773c529cad0e317
ronycode <ronypc@outlook.com>
2023-10-12 21:07:33 -0300

    primeiro projeto arch

--- src/Web/Message/receiver.php
+++ src/Web/Message/receiver.php
@@ -8,7 +8,7 @@ try {
     $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
     $channel    = $connection->channel();
 
-    $channel->queue_declare('hello', false, false, false, false);
+    $channel->queue_declare('LogUserSigned', false, false, false, false);
 
     echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
